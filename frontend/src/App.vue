be93b33c80d5e8cb9a0c3a2790e6f8578169ddf4
burak48 <buraksaglam3@gmail.com>
2022-02-27 00:05:28 +0300

    footer & question component added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
     <nav-bar />
+    <question-component />
+    <footer-component />
   </div>
 </template>
 
 <script>
 import NavBar from './components/Navbar.vue';
+import QuestionComponent from './components/Question.vue';
+import FooterComponent from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
     NavBar,
+    QuestionComponent,
+    FooterComponent,
   },
 };
 </script>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,69 @@
+<template>
+  <footer>
+    <div class="row copyright">
+      <div class="footer-menu">
+        <a href="">Home</a>
+        <a href="">About</a>
+      </div>
+      <p>Copyright &copy; 2022 Burak Saglam</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+footer {
+  background-color: #121315;
+  color: #a7a7a7;
+  font-size: 16px;
+}
+footer * {
+  font-family: 'Poppins', sans-serif;
+  box-sizing: border-box;
+  border: none;
+  outline: none;
+}
+.row {
+  padding: 1em 1em;
+}
+.row.primary {
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr 2fr;
+  align-items: stretch;
+}
+.copyright {
+  padding: 0.3em 1em;
+  background-color: #25262e;
+}
+.footer-menu {
+  float: left;
+  margin-top: 10px;
+}
+
+.footer-menu a {
+  color: #cfd2d6;
+  padding: 6px;
+
+  text-decoration: none;
+}
+.footer-menu a:hover {
+  color: #27bcda;
+}
+.copyright p {
+  font-size: 0.9em;
+  text-align: right;
+}
+@media screen and (max-width: 850px) {
+  .row.primary {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
